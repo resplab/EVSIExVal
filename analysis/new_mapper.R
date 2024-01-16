06d98d0b73bead635f14b5c0f936f43490a7a275
Sadatsafavi <mohsen.sadatsafavi@ubc.ca>
2024-01-15 16:20:02 -0800

    Updates

--- analysis/new_mapper.R
+++ analysis/new_mapper.R
@@ -47,11 +47,11 @@ logit <- function(x) {log(x/(1-x))}
 expit <- function(x) {1/(1+exp(-x))}
 
 
-pdf <- function(x, mu, sigme) {1/sigma/sqrt(2*pi)/x/(1-x)*exp(-(logit(x)-mu)^2/(2*sigma^2))}
-cdf <- function(x, mu, sigme) {pnorm(logit(x),mu,sigma)}
+pdf <- function(x, mu, sigma) {1/sigma/sqrt(2*pi)/x/(1-x)*exp(-(logit(x)-mu)^2/(2*sigma^2))}
+cdf <- function(x, mu, sigma) {pnorm(logit(x),mu,sigma)}
 
-mu <- 2
-sigma <- 10
+mu <- -0.4515322
+sigma <- 0.7134626
 
 x <- (1:1000)/1001
 plot(x, pdf(x,mu,sigma), type='l')
@@ -70,3 +70,29 @@ sum(expit(qnorm(i/K,mu,sigma)))/(K-1)
 
 integrate(function(x,mu,sigma){x*pdf(x,mu,sigma)},0,1,mu,sigma)$value
 
+#########################################################
+
+m <- 0.4
+c <- 0.7
+
+F1 <- 1-m
+
+D <- sqrt((1-2*c)*m*m - (1-2*c)*m + 0.25)
+
+F2 <- 1-c(m-0.5+D, m-0.5-D)[1]
+
+pdf <- function(x, mu, sigma) {1/sigma/sqrt(2*pi)/x/(1-x)*exp(-(logit(x)-mu)^2/(2*sigma^2))}
+cdf <- function(x, mu, sigma) {pnorm(logit(x),mu,sigma)}
+cdf2 <- function(x, mu, sigma) {pnorm(logit(x),mu,sigma)^2}
+
+
+f <- function(x)
+{
+  f1 <- integrate(cdf,0,1,mu=x[1],sigma=x[2])$value
+  f2 <- integrate(cdf2,0,1,mu=x[1],sigma=x[2])$value
+
+  (f1-F1)^2+(f2-F2)^2
+}
+
+
+optim(c(0,1), f)
